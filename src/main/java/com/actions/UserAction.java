8a3f4de8ade5cb86d0efc5a7760dc35f0239c658
dhisinha <dhisinha@cisco.com>
2015-04-24 19:33:56 +0530

    1st commit

---
+++ src/main/java/com/actions/UserAction.java
@@ -0,0 +1,21 @@
+package com.actions;
+
+import com.bean.User;
+
+public class UserAction {
+	
+	private User user;
+
+	public User getUser() {
+		return user;
+	}
+
+	public void setUser(User user) {
+		this.user = user;
+	}
+	
+	public String execute() {
+		return "success";
+	}
+
+}

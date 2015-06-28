b1291338b88c95e4d4451cf684e5b33c2a52643e
Dhiraj Sinha <dhiraj.ach17@gmail.com>
2015-06-28 15:04:46 +0530

    modified for while loop in Thread

--- src/main/java/com/Thread/InterThreadCommunication/Accoount.java
+++ src/main/java/com/Thread/InterThreadCommunication/Accoount.java
@@ -4,27 +4,29 @@ public class Accoount {
 	int balance;
 
 	public synchronized void debitAccount(int amount) {
+		try {
 
-		System.out.println("Checking balance");
-		while ((balance < amount)) {
-			System.out.println("Going to wait");
-			try {
+			System.out.println("Checking balance");
+			while ((balance < amount)) {
+				System.out.println("Going to wait");
+				System.out.println("Going to wait1");
 				wait();
-			} catch (InterruptedException e) {
-				e.printStackTrace();
+				System.out.println("Going to wait2");
 			}
+
+			System.out.println("Debeting...");
+			balance = balance - amount;
+
+			System.out.println("Balance after debeting : " + balance);
+		} catch (InterruptedException e) {
+			e.printStackTrace();
 		}
-
-		System.out.println("Debeting...");
-		balance = balance - amount;
-
-		System.out.println("Balance after debeting : " + balance);
 
 	}
 
 	public synchronized void creditAccount(int amount) {
-		balance+=amount;
-		System.out.println("Balance = "+balance+"notify");
+		balance += amount;
+		System.out.println("Balance = " + balance + "notify");
 		notify();
 	}
 
